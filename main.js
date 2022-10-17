5a238b02dc0909872a0d29dd36223d0cc34f9f89
Mayank-TN <mtandon10@gmail.com>
2022-10-17 11:06:24 +0530

    getElementByTagName

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 
-const items = document.getElementsByClassName('list-group-item');
-
+//const items = document.getElementsByClassName('list-group-item');
+const items = document.getElementsByTagName('li');
 
 console.log(items)
 
 items[2].style.backgroundColor = 'green';
 
 
-for(let i=0;i<4;i++){
+for(let i=0;i<items.length;i++){
     items[i].style.fontWeight = "bold";
 }
+
+console.log(items[4]);
