f376c695a6c3abdc37abaffe312a635312770c5e
Mayank-TN <mtandon10@gmail.com>
2022-10-17 11:24:29 +0530

    querySelector

--- main.js
+++ main.js
@@ -12,3 +12,13 @@ for(let i=0;i<items.length;i++){
 }
 
 console.log(items[4]);
+
+const item2 = document.querySelector('.list-group-item:nth-child(1)');
+item2.style.color = 'green';
+
+const odds = document.querySelectorAll('.list-group-item:nth-child(odd)');
+
+console.log(odds)
+for(let i=0;i<odds.length;i++){
+    odds[i].style.backgroundColor = 'green'
+}
